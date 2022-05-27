e6540ea1cc0844d99b52ea6bb744f30e63e58057
oreinventor <63708901+oreinventor@users.noreply.github.com>
2022-05-27 14:32:50 -0300

    Add files via upload

--- updatedate.bat
+++ updatedate.bat
@@ -1,4 +1,6 @@
 @echo off
+date 01-06-2022
 net stop w32time
+w32tm /config /reliable:YES /manualpeerlist:"ntps1.pads.ufrj.br time.google.com time.cloudflare.com time.facebook.com time.windows.com time.apple.com clepsydra.dec.com time-a-g.nist.gov" /syncfromflags:manual /update
 net start w32time
-w32tm /resync
+w32tm /resync /rediscover /force
