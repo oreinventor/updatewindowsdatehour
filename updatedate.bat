983c73cb5e3e2a659ff40c8dc1a0bf888122aa22
oreinventor <63708901+oreinventor@users.noreply.github.com>
2022-05-27 15:29:55 -0300

    Update updatedate.bat

--- updatedate.bat
+++ updatedate.bat
@@ -3,4 +3,4 @@ date 01-06-2022
 net stop w32time
 w32tm /config /reliable:YES /manualpeerlist:"ntps1.pads.ufrj.br time.google.com time.cloudflare.com time.facebook.com time.windows.com time.apple.com clepsydra.dec.com time-a-g.nist.gov" /syncfromflags:manual /update
 net start w32time
-w32tm /resync /rediscover /force
+w32tm /resync /force
